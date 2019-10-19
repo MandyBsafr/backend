f9dd8d2d3228308dae482022a54efd4f42907d33
PushBtnsDev <kyledchenoweth@gmail.com>
2019-10-19 12:14:03 +1100

    setting up heroku dev server

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
 const express = require('express');
-const morgan = require('morgan');
+// const morgan = require('morgan');
 const cors = require('cors');
 const app = express();
 
 const PORT = process.env.PORT || 5000;
-app.use(morgan('dev'))
+// app.use(morgan('dev'))
 app.use(express.json())
 app.use(cors());
 
