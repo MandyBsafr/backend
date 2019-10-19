f232f0d89b0c0badb44ed754ecae605a04ef7f2a
PushBtnsDev <kyledchenoweth@gmail.com>
2019-10-20 00:13:05 +1100

    sms api core functionality complete

--- routes/users.js
+++ routes/users.js
@@ -3,37 +3,6 @@ const router = express.Router();
 const mongoose = require('mongoose');
 const User = require('../models/User');
 
-// TODO:
-// SMS ami hookup
-// endpoint for end session / mark for deletion
-
-router.get('/emergency/:id', async (req, res, next) => {
-
-  try {
-    const user = await User.findById(req.params.id);
-    console.log(user.contacts[0]);
-    const accountSid = process.env.SMS_ACC;
-    const authToken = process.env.AUTH_TOKEN;
-    const client = require('twilio')(accountSid, authToken);
-    client.messages
-    .create({
-      body: `BSAFE ALERT
-      I have failed to check out on time!
-      `,
-      from: '+17344695155',
-      to: `+61${user.contacts[0]}`
-    })
-    .then(message => {
-      console.log(message.sid)
-      res.send(message);
-    });
-    res.send('Successful text');
-  }
-  catch (err) {
-    res.status(500).send(err);
-  }
-})
-
 router.get('/:id', async (req, res, next) => {
   // GET user by id
   console.log(req.params.id);
@@ -75,6 +44,38 @@ router.post('/create', async (req, res, next) => {
   }
 
   res.send(`Created user: ${user}`);
+})
+
+router.post('/emergency/:id', async (req, res, next) => {
+  // user location latitude, longitude
+  const { latitude, longitude } = req.body
+  try {
+    const user = await User.findById(req.params.id);
+
+    // twilio sms send message to emergency contact
+    const accountSid = process.env.SMS_ACC;
+    const authToken = process.env.AUTH_TOKEN;
+    const client = require('twilio')(accountSid, authToken);
+
+    client.messages
+    .create({
+      body: `BSAFE ALERT
+      I have failed to check out on time!
+      At time: TIME CHECK IN
+      Location: https://www.google.com/maps/?q=${user.location.coordinates[1]},${user.location.coordinates[0]}
+      Last known location:
+      Location: https://www.google.com/maps/?q=${latitude},${longitude}`,
+      from: '+17344695155',
+      to: `+61${user.contacts[0]}`
+    })
+    .then(message => {
+      console.log(message.sid)
+      res.send(message);
+    });
+  }
+  catch (err) {
+    res.status(500).send(err);
+  }
 })
 
 module.exports = router;
